849fa6fa65dec2c00f239599562295c96cc1b8cf
KennethBerglund <133186845+KennethBerglund@users.noreply.github.com>
2023-06-06 19:43:55 -0400

    first commit - random choice function
    Created a function that uses Math.random to pick a random string from an array.

---
+++ rock paper sciossors/cool.js
@@ -0,0 +1,10 @@
+
+
+                 //// //* Random choice function*//
+
+let arr = ["rock", "paper", "scissor"];
+
+function random() {
+    console.log(arr[(Math.floor(Math.random() * arr.length))]);
+}
+random()
