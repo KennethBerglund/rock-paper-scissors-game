6501b27282a92a2802bb1da1559722f891554995
KennethBerglund <133186845+KennethBerglund@users.noreply.github.com>
2023-06-07 12:58:38 -0400

    functionality- player, computer, one round
    added function for player choice
    added function for computer choice
    
    added function for playing one round of game
    (output is still not what is to be expected)

--- rock paper sciossors/cool.js
+++ rock paper sciossors/cool.js
@@ -1,10 +1,61 @@
+//                     ///*PLAYER input*/////
 
+let person = prompt("enter rock paper or scissors");
+function playerSelection() {
+    return(person);
+}
+
+let userchoice = person;
+
+console.log(userchoice);
 
                  //// //* Random choice function*//
+                       ///* COMPUTER*////
 
-let arr = ["rock", "paper", "scissor"];
 
-function random() {
-    console.log(arr[(Math.floor(Math.random() * arr.length))]);
+let computer = ["rock", "paper", "scissor"];
+function computerSelection() {
+    console.log(computer[(Math.floor(Math.random() * computer.length))]);
+    return(computer);}
+
+computerSelection(computer);
+
+
+
+
+                
+
+// //                     //////* 1 round of game*////
+
+function playRound(computerSelection, playerSelection) {
+     if(computer === person) {
+     return("tie");}}
+//     }else if (computerSelection === "rock" && playerSelection === "scissors"){
+//         return("computer");
+//     }else if (computerSelection === "scissors" && playerSelection === "rock"){
+//         return("player");
+//     }else if(computerSelection === "paper" && playerSelection === "rock"){
+//         return("computer");
+//     }else if (computerSelection === "rock" && playerSelection === "paper"){
+//         return("player");
+//     }else if (computerSelection === "paper" && playerSelection === "scissors"){
+//         return("player");
+//     }else if (computerSelection === "scissors" && playerSelection === "paper"){
+//         return("computer"); } {
+//             console.log("you lose!!!");
+//     }
+// };
+playRound();
+
+let result = playRound(computerSelection, playerSelection);
+
+if (result === "player"){
+    alert ("you won!");
 }
-random()
+else if (result === "computer"){
+    alert ("you lose");
+}
+else if (result === tie){
+    alert ("tie!")
+}
+
