d18512a30df2297fc230ca7d5f989d3333d349b4
KennethBerglund <133186845+KennethBerglund@users.noreply.github.com>
2023-06-08 14:58:18 -0400

    Rock, Paper, Scissors!-added-game winner loop break
    added conditionals that stop the for loop when the player or computer score === 3 
    this conditional will also log (GAME OVER!) 
    added alert(PLAYER WINs! or COMPUTER WINS!) depending on which player reaches  a score of 3 first.
    console logs (Game Over!) and final score displayed then insert a (break;) to exit the loop and prevent playRound() from executing and showing prompt.

---
+++ cool.v5.js
@@ -0,0 +1,86 @@
+const choices = ["rock", "paper", "scissors"];
+const winners= [];
+
+
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+            ////*PLAY GAME*/////
+function playGame() {
+   for(i = 0; i <= 10; i++)
+   if(computerScore === 3 || playerScore === 3){
+    console.log("Game Over!");
+    if(computerScore === 3){alert("COMPUTER WON! YOU LOSE!");}
+    else if(playerScore === 3) {alert("PLAYER WON!");break;}
+   }else if(computerScore === 3){alert('COMPUTER WINS!');break;}
+   else if(playerScore === 3){alert('PLAYER WINS!');}
+   else if(computerScore === 3 || playerScore === 3){break;}
+   else{
+    playRound();
+   }
+}
+
+            ///*PLAY ROUND*/////
+function playRound(){
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    console.log(winner);
+    if(winner === "player"){
+        playerScore++;
+    }else if(winner === "tie"){
+        ties++;
+    }else{
+        computerScore++;
+    }
+
+    console.log("player:", playerScore);
+    console.log ("computer:", computerScore);
+    console.log("ties:", ties);
+}
+
+            ///*PLAYER CHOICE*///
+function playerChoice() {
+let input = prompt("select rock, paper, or scissors");
+    console.log(input);
+    let choiceP = input;
+    return (choiceP);
+}
+
+            /*COMPUTER CHOICE*///
+function computerChoice() {
+    let choice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(choice);
+    return (choice);
+}
+
+function checkWinner(choiceP, choice) {
+if(choiceP === choice){
+    return "tie";
+}else if(
+    (choiceP === "rock" && choice === "scissors") ||
+    (choiceP === "paper" && choice === "rock") || 
+    (choiceP === "scissors" && choice === "paper")
+){ 
+return "player";
+}else{
+    return "computer";
+}
+}
+
+function gameScore(winner) {
+        if(winner === "player"){
+            playerScore++;
+        }else if(winner === "tie"){
+            ties++;
+        }else{
+            computerScore++;
+    }
+    }
+if(playerScore === 3){
+    console.log("player")
+
+}
+playGame();
